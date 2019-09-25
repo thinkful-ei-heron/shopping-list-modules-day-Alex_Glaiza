ce01a2b13f45c0586cce47b17c5d0366a57e81a3
Glaiza Wagner <glaizawagner@gmail.com>
2019-09-25 08:48:50 -0700

    4 wireup item methods to be use in shopping-list

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -43,7 +43,16 @@ const render = function () {
 };
 
 const addItemToShoppingList = function (itemName) {
-  store.items.push({ id: cuid(), name: itemName, checked: false });
+  //store.items.push({ id: cuid(), name: itemName, checked: false });
+  try {
+    item.validateName(itemName);
+    item.create(itemName);
+    store.items.push(item.create(itemName));
+    render();
+  }catch(error) {
+    console.log(`Cannot add item: ${error.messsage}`);
+  }
+
 };
 
 const handleNewItemSubmit = function () {
