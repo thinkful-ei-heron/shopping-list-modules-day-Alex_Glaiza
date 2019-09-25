eab5500e56422f588f3e8048108752d26b82f9cf
Alex Bannow <rbannal@gmail.com>
2019-09-25 11:37:51 -0400

    modified item.js according the part 3

--- scripts/item.js
+++ scripts/item.js
@@ -1,1 +1,11 @@
-export default {};
+const validateName = function(name) {
+  if(name === ''){
+    throw TypeError('Name must not be blank');
+  }
+  return name;
+};
+const create = function(name) {
+  const item = {id: cuid(), name: name, checked: false};
+  return item;
+};
+export default { validateName, create };
